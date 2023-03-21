91b02c64c3d535d934b492e1f858e4f8033c9cd4
truongvq <truongvq@viettel.com.vn>
2023-03-21 18:22:00 +0700

    Update docker image

--- src/main/scala/com/viettel/truongvq/doc/airflow.md
+++ src/main/scala/com/viettel/truongvq/doc/airflow.md
@@ -1,2 +1,44 @@
 # Apache Airflow
+Apache Airflow is an open-source platform for developing, scheduling, and monitoring batch-oriented workflows.
+
+The main characteristic of Airflow workflows is that all workflows are defined in Python code. “Workflows as code” serves several purposes:
+
+- Dynamic: Airflow pipelines are configured as Python code, allowing for dynamic pipeline generation.
+
+- Extensible: The Airflow framework contains operators to connect with numerous technologies. All Airflow components are extensible to easily adjust to your environment.
+
+- Flexible: Workflow parameterization is built-in leveraging the Jinja templating engine.
+
+# Fundamental Concept
+
+## DAG file (Directed Acyclic Graph)
+A configuration file specifying the DAG structure as code.
+The Python script purpose is to define DAG object
+
+```
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG(dag_id="FirstDAG", start_date=datetime(2023, 3, 20), schedule_interval="@hourly",
+         catchup=False) as dag:
+
+    task = BashOperator(
+        task_id="print_date",
+        bash_command="date",)
+
+task
+```
+
+## Operators
+Using operators helps to visualize task dependencies in our DAG code.
+
+All operators inherit from the BaseOperator, which includes all of the required arguments for running work in Airflow.
+
+Airflow completes work based on the arguments you pass to your operators.
+
+## Task
+To use an operator in a DAG, you have to instantiate it as a task. Tasks determine how to execute your operator’s work within the context of a DAG.
+
+[//]: # (Notice how we pass a mix of operator specific arguments &#40;bash_command&#41; and an argument common to all operators &#40;retries&#41; inherited from BaseOperator to the operator’s constructor. This is simpler than passing every argument for every constructor call. Also, notice that in the second task we override the retries parameter with 3.)
 
--- src/main/scala/com/viettel/truongvq/spark/Spark.scala
+++ src/main/scala/com/viettel/truongvq/spark/Spark.scala
@@ -11,33 +11,33 @@ object Spark extends App {
   val myCollection = "Spark The Definitive Guide : Big Data Processing Made Simple"
     .split(" ")
   val words = spark.sparkContext.parallelize(myCollection, 2)
-  val keyword = words.keyBy(word => word.toLowerCase.toSeq(0).toString)
+  val keyword = words.keyBy(word => word.toLowerCase.toSeq.head.toString)
   print(keyword.lookup("s"))
-  val chars = words.flatMap(word => word.toLowerCase.toSeq)
-  val KVcharacters = chars.map(letter => (letter, 1))
-  print(KVcharacters.countByKey())
-  print(KVcharacters.groupByKey().map(row => (row._1, row._2.sum)).collect().mkString("Array(", ", ", ")"))
-  print(KVcharacters.reduceByKey((x,y) => x + y).collect().mkString("Array(", ", ", ")"))
-  println(words.countByValue())
-  println(words.map(word => (word, word(0))).collect().mkString("Array(", ", ", ")"))
-  println(words.mapPartitions(iterator => iterator.map(word => (word, word(0)))).collect().mkString("Array(", ", ", ")"))
-  words.foreachPartition { iter =>
-    while (iter.hasNext) {
-      println(iter.next())
-    }
-  }
-  val word2 = words.glom().collect()
-  val nums = spark.sparkContext.parallelize(1 to 30, 5)
-  print(nums.aggregate(0)((x, y) => x.max(y), (x,y) => x + y))
-  print(KVcharacters.aggregateByKey(0)((x, y) => x + y, (x, y) => x.max(y)).collect().mkString("Array(", ", ", ")"))
-  print(KVcharacters.foldByKey(0)((x, y) => x + y).collect().mkString("Array(", ", ", ")"))
-  val distinctChars = words.flatMap(word => word.toLowerCase.toSeq).distinct
-  val keyedChars = distinctChars.map(c => (c, 0))
-  val outputPartitions = 10
-  println(KVcharacters.join(keyedChars).count())
-  println(KVcharacters.leftOuterJoin(keyedChars, outputPartitions).collect().mkString("Array(", ", ", ")"))
-  println(keyedChars.count())
-  println(KVcharacters.coalesce(3).getNumPartitions)
-  println()
+//  val chars = words.flatMap(word => word.toLowerCase.toSeq)
+//  val KVcharacters = chars.map(letter => (letter, 1))
+//  print(KVcharacters.countByKey())
+//  print(KVcharacters.groupByKey().map(row => (row._1, row._2.sum)).collect().mkString("Array(", ", ", ")"))
+//  print(KVcharacters.reduceByKey((x,y) => x + y).collect().mkString("Array(", ", ", ")"))
+//  println(words.countByValue())
+//  println(words.map(word => (word, word(0))).collect().mkString("Array(", ", ", ")"))
+//  println(words.mapPartitions(iterator => iterator.map(word => (word, word(0)))).collect().mkString("Array(", ", ", ")"))
+//  words.foreachPartition { iter =>
+//    while (iter.hasNext) {
+//      println(iter.next())
+//    }
+//  }
+//  val word2 = words.glom().collect()
+//  val nums = spark.sparkContext.parallelize(1 to 30, 5)
+//  print(nums.aggregate(0)((x, y) => x.max(y), (x,y) => x + y))
+//  print(KVcharacters.aggregateByKey(0)((x, y) => x + y, (x, y) => x.max(y)).collect().mkString("Array(", ", ", ")"))
+//  print(KVcharacters.foldByKey(0)((x, y) => x + y).collect().mkString("Array(", ", ", ")"))
+//  val distinctChars = words.flatMap(word => word.toLowerCase.toSeq).distinct
+//  val keyedChars = distinctChars.map(c => (c, 0))
+//  val outputPartitions = 10
+//  println(KVcharacters.join(keyedChars).count())
+//  println(KVcharacters.leftOuterJoin(keyedChars, outputPartitions).collect().mkString("Array(", ", ", ")"))
+//  println(keyedChars.count())
+//  println(KVcharacters.coalesce(3).getNumPartitions)
+//  println()
 
 }
