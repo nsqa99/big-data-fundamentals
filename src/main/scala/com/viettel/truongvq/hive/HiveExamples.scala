0a7ed525ee91024f3049622d6aa24569c50d679b
truongvq <truongvq@viettel.com.vn>
2023-04-13 13:53:26 +0700

    Update sql

--- src/main/scala/com/viettel/truongvq/hive/HiveExamples.scala
+++ src/main/scala/com/viettel/truongvq/hive/HiveExamples.scala
@@ -8,7 +8,7 @@ object HiveExamples extends App {
 //  var sql = "SELECT count(*) FROM flight_data_temp f JOIN airports a JOIN airports p"
 //  var sql = "SELECT `arr.*` FROM flight_data_temp LIMIT 1"
 //  var sql = "SELECT * FROM airports WHERE name RLIKE '.*(City|County).*'"
-  var sql = "SELECT a1.name, count(1), sum(air_time) FROM flight_data_by_date f\n" + "JOIN airports a1 ON a1.code = f.origin\n" + "JOIN airports a2 ON a2.code = f.dest\n" + "WHERE distance > 1000 and a2.country = 'US'\n" + "GROUP BY a1.name";
+  var sql = "SELECT a1.name, count(1), sum(air_time) FROM flight_data_temp f\n" + "JOIN airports_temp a1 ON a1.code = f.origin\n" + "JOIN airports_temp a2 ON a2.code = f.dest\n" + "WHERE distance > 1000 and a2.country = 'US'\n" + "GROUP BY a1.name";
   var con = DriverManager.getConnection(conStr);
   try {
     val stmt = con.createStatement()
